3540c69b0a82b02780f18952bf6208d70a1f4080
Baruch Guerra <baruch.gr@hotmail.co>
2024-11-04 20:08:19 -0600

    FEAT: Added last exercise from the first week of Java diploma

--- EjerciciosClaseMath/src/main/java/org/mathclass/FourthExercise.java
+++ EjerciciosClaseMath/src/main/java/org/mathclass/FourthExercise.java
@@ -1,4 +1,19 @@
 package org.mathclass;
 
 public class FourthExercise {
+    public static void main(String[] args) {
+        double number1 = (Math.random() * 50) + 1;
+        double number2 = (Math.random() * 50) + 1;
+        System.out.printf("Numbers: " + number1);
+        System.out.println(" " + number2);
+        System.out.println("Round method: " + Math.round(number1));
+        System.out.println("Round method: " + Math.round(number2));
+
+        System.out.println("Floor method: " + Math.floor(number2));
+        System.out.println("Floor method: " + Math.floor(number1));
+
+        System.out.println("Ceil method: " + Math.ceil(number1));
+        System.out.println("Ceil method: " + Math.ceil(number2));
+
+    }
 }
