7daaff035924e5c57821fa636594d180b5e244a4
Baruch Guerra <baruch.gr@hotmail.co>
2024-11-18 21:40:32 -0600

    FIX: Removed unused code

--- LoopsExercises/src/main/java/org/loopsexercises/Dividers.java
+++ LoopsExercises/src/main/java/org/loopsexercises/Dividers.java
@@ -4,20 +4,15 @@ import java.util.Stack;
 
 public class Dividers {
     public static void main(String[] args) {
-        int numberToFindDividers = 90;
-        Stack<Integer> dividersStack = new Stack<>();
-        for(int i = 1; i <= numberToFindDividers; i++){
-            if (numberToFindDividers % i == 0) {
-                dividersStack.push(i);
-            }
-        }
+        int numberToFindDividers = 20;
+        Stack<Integer> stackOfDividers = getDividers(numberToFindDividers);
 
-        for(int divider: dividersStack) {
+        for(int divider: stackOfDividers) {
             System.out.println("Divider: " + divider);
         }
     }
 
-    public Stack<Integer> getDividers(int numberToFindDividers) {
+    public static Stack<Integer> getDividers(int numberToFindDividers) {
         Stack<Integer> dividersStack = new Stack<>();
         for(int i = 1; i <= numberToFindDividers; i++){
             if (numberToFindDividers % i == 0) {
